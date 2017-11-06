78e129fcd0abc26a4f4b249464d0d75774c7d301
Chandler Morrison <cwm795@gmail.com>
2017-11-05 22:06:39 -0600

    finished exercise, final commit

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
-console.log("hi");
+
+console.log ("hi")
+
+
+
+
+// Create an array that contains the words in the sentence
+
+
+var sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+
+// The addExcitement function should accept the array as the sole argument
+
+
+function addExcitement (theWordArray) {
+
+
+   /*
+
+
+     Write a `for` loop that iterates over the array argument and
+
+
+     outputs the words.
+
+
+    */
+
+
+    let arrString = "";
+    let exclamation = "!";
+
+    for (i = 0; i < sentence.length; i++) {
+
+    	sentence[i]
+
+    	if (i % 3 === 2) {
+
+
+    		arrString += " " + sentence[i] + exclamation;
+
+    		exclamation += "!"
+
+
+    	} else {
+
+
+    			arrString = arrString + " " + sentence[i];
+
+    	}
+
+    }
+
+
+}
+
+
+// Invoke the function and pass in the array
+
+
+addExcitement(addExcitement);
+
